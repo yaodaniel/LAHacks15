27bc6c04026a155a2c493ace25664dcf136b74ef
Ian P. Cordero <ianpcordero@gmail.com>
2015-04-04 13:32:00 -0700

    Bug fixes

--- play.php
+++ play.php
@@ -20,8 +20,6 @@ $id = $_GET['id'];
     // "Constants"
     var songID = <?php echo $id ?>;
 
-    function main()
-    {
         SC.stream("/tracks/" + songID, function(sound) {
                   // Initialize canvas
             console.log("Initializing canvas");
@@ -125,20 +123,12 @@ $id = $_GET['id'];
             });
         });
         
-        SC.stream("/tracks/" + songID, function(sound) {
-                  sound.play();
-        });
-    }
-
-    $(document).ready(main);
-
 	</script>
 </head>
-	
+<body>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-</div>
 </body>
 </html>
