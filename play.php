bd821a9b341aa3e31fee91b02a1c514e6b6f25fd
yaodaniel <Daniel988@ucla.edu>
2015-04-04 20:12:58 -0700

    added new images for index.html and changed play.php

--- play.php
+++ play.php
@@ -15,117 +15,65 @@ $id = $_GET['song'];
     <script src="js/dsp.js"></script>
     <!-- Three.js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r71/three.js"></script>
-	<script>SC.initialize({ client_id: '0a25f7c9ec955ced6294e9e5dcbbb532'});</script>
+	<!--<script>SC.initialize({ client_id: '0a25f7c9ec955ced6294e9e5dcbbb532'});</script>-->
+	<script>
+	var SoundCloudAudioSource = function(player) {
+		var self = this;
+		var analyser;
+		var audioCtx = new (window.AudioContext || window.webkitAudioContext);
+		analyser = audioCtx.createAnalyser();
+		analyser.fftSize = 256;
+		var source = audioCtx.createMediaElementSource(player);
+		source.connect(analyser);
+		analyser.connect(audioCtx.destination);
+		var sampleAudioStream = function() {
+			analyser.getByteFrequencyData(self.streamData);
+			// calculate an overall volume value
+			var total = 0;
+			for (var i = 0; i < 80; i++) { // get the volume from the first 80 bins, else it gets too loud with treble
+				total += self.streamData[i];
+			}
+			self.volume = total;
+		};
+		setInterval(sampleAudioStream, 20);
+		// public properties and methods
+		this.volume = 0;
+		this.streamData = new Uint8Array(128);
+		this.playStream = function(streamUrl) {
+			// get the input stream from the audio element
+			player.addEventListener('ended', function(){
+				self.directStream('coasting');
+			});
+			player.setAttribute('src', streamUrl);
+			player.play();
+		}
+	};
+	</script>
 	<script>
     // "Constants"
-    var songID = <?php echo $id ?>;
-
-        SC.stream("/tracks/" + songID, function(sound) {
-                  // Initialize canvas
-            console.log("Initializing canvas");
-                  
-            // Configuration variables
-            var numCubes = 100;
-            var defaultCameraPosition = { x: 0, y: 0, z: 10 };
-              var defaultCubePosition = { x: -15, y: 0, z: 0 };
-              var startingWidthBound = 15;
-              var averageVelocity = 0.25;
-              var initialDirection = { x: 1, y: 0 };
-              var defaultCubeSize = 1;
-              
-              // State variables
-              var isPlaying = true;
-              var currentDirection = { x: 1, y: 0 }; // x, y := { -1, 0, 1 }
-
-            // Initialize scene
-            var scene = new THREE.Scene();
-            var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-            var renderer = new THREE.WebGLRenderer();
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            document.body.appendChild(renderer.domElement);
-                  camera.position.z = defaultCameraPosition.z;
-
-            // Set up the scene
-            var cubes = [];
-                  
-                  // Set up lighting
-                  var ambientLight = new THREE.AmbientLight(0x808080);
-                  var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.5);
-                  
-                  // Add lights to scene
-                  scene.add(directionalLight);
-                  scene.add(ambientLight);
-
-                  // Add cubes to scene
-            for (var i = 0; i < numCubes; i++)
-            {
-                  // Set up cube
-                var geometry = new THREE.BoxGeometry(defaultCubeSize, defaultCubeSize, defaultCubeSize);
-                var material = new THREE.MeshLambertMaterial({ color: 0xCC0000 });
-                var cube = new THREE.Mesh(geometry, material);
-                  
-                  cube.position.x = defaultCubePosition.x + (-startingWidthBound + Math.random() * startingWidthBound * 2);
-                  cube.position.y = defaultCubePosition.y;
-                  cube.position.z = defaultCubePosition.z;
-                  
-                  // Add cube to scene
-                  scene.add(cube);
-                  
-                  // Add cube to array for future reference
-                cubes.push(cube);
-            }
-                  
-                  // Configure render behavior
-            function render()
-            {
-                  // Schedule next frame
-                if (isPlaying)
-                {
-                  requestAnimationFrame(render);
-                  renderer.render(scene, camera);
-                }
-                  
-                // Update scene as appropriate using variables that whileplaying() updates
-                
-            }
-                  
-            // Call first render
-            render();
-                  
-                  // Configure update function for sound
-                  
-                  // Updates variable state to reflect the music signal's "spectrum" transform
-            sound.whileplaying = function() {
-                // Manipulate canvas
-                console.log("Manipulating canvas");
-
-                  // Reference for DSP.js
-                  //var fft = new FFT(/* bufferSize: */ 2048, /* sampleRate: */ 44100);
-                  //fft.forward(signal);
-                  //var spectrum = fft.spectrum;
-                  
-                // Reference for SM2 waveformdata
-                //for (var i=0; i<256; i++) {
-                //graphPixels[i].style.top = (gScale+Math.ceil(this.waveformData.left[i]*-gScale))+'px';
-                //}
-            };
-                  
-            // Play sound
-            sound.play({
-                // Responsible for resetting animations and performing other clean-up.
-                onfinish: function() {
-                    // Reset canvas
-                    console.log("Resetting canvas");
-
-                    // Update state to reflect that sound is no longer playing
-                    isPlaying = false;
-                }
-            });
-        });
-        
+	var SoundcloudLoader = function(player) {
+		var self = this;
+		this.player = player;
+		SC.initialize({ client_id: '0a25f7c9ec955ced6294e9e5dcbbb532'});
+		var songUrl = <?php echo $id ?>;
+		self.streamUrl = audioTagSrc = songUrl + '?client_id=' + client_id;
+		
+		this.directStream = function(direction){
+			if(direction=='toggle'){
+				this.player.play();
+			} else {
+				this.player.pause();
+			}
+		}
+		this.player.setAttribute('src',this.streamUrl);
+		this.player.play();
+	}
 	</script>
 </head>
 <body>
+	<canvas width="1600" height="408" style="height:80vh; width:100vw">
+	<audio id="player" controls="" autoplay="" preload autobuffer></audio>
+	
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
