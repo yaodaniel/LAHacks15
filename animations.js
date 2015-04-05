206f5742428b7add32e743b715cfeecc5bd80800
PGrad-Icarus <deexiguavenitlux@gmail.com>
2015-04-05 03:59:55 -0700

    Added fireworks animation
    Music visualization that syncs ebb and flow of "sparks" with beat of music.

--- animations.js
+++ animations.js
@@ -205,3 +205,172 @@ function cubeAnimation()
     audioSource.playStream(loader.streamUrl);
     draw();
 }
+
+function fireworks() {
+	if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+	
+	var player = document.getElementById('player');
+    var canvasElement = document.getElementById('canvas');
+    var context = canvasElement.getContext("2d");
+    var loader = new SoundcloudLoader(player);
+    var audioSource = new SoundCloudAudioSource(player);
+				
+	var SCREEN_WIDTH = window.innerWidth,
+	SCREEN_HEIGHT = window.innerHeight,
+    r = 450,
+	mouseX = 0, mouseY = 0,
+	windowHalfX = window.innerWidth / 2,
+	windowHalfY = window.innerHeight / 2,
+	camera, scene, renderer;
+	
+	init();
+	animate();
+	
+	function init() {
+	
+    	var container;
+    	
+    	container = document.createElement( 'div' );
+    	document.body.appendChild( container );
+    	
+    	camera = new THREE.PerspectiveCamera( 80, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 3000 );
+    	camera.position.z = 1000;
+    	
+    	scene = new THREE.Scene();
+    	
+    	var i, line, vertex1, vertex2, material, p,
+    	parameters = [ [ 0.25, 0xff7700 * Math.random(), 1, 2 ], [ 0.5, 0xff9900 * Math.random(), 1, 1 ], [ 0.75, 0xffaa00 * Math.random(), 0.75, 1 ], [ 1, 0xffaa00 * Math.random(), 0.5, 1 ], [ 1.25, 0x000833 * Math.random(), 0.8, 1 ],
+    				[ 3.0, 0xaaaaaa * Math.random(), 0.75, 2 ], [ 3.5, 0xffffff * Math.random(), 0.5, 1 ], [ 4.5, 0xffffff * Math.random(), 0.25, 1 ], [ 5.5, 0xffffff * Math.random(), 0.125, 1 ] ],
+    	
+    	geometry = new THREE.Geometry();
+    	
+    	for ( i = 0; i < 1500; i ++ ) {
+    	
+    		var vertex1 = new THREE.Vector3();
+    		vertex1.x = Math.random() * 2 - 1;
+    		vertex1.y = Math.random() * 2 - 1;
+    		vertex1.z = Math.random() * 2 - 1;
+    		vertex1.normalize();
+    		vertex1.multiplyScalar( r );
+    	
+    		vertex2 = vertex1.clone();
+    		vertex2.multiplyScalar( Math.random() * 0.09 + 1 );
+    	
+    		geometry.vertices.push( vertex1 );
+    		geometry.vertices.push( vertex2 );
+    	
+    	}
+    	
+    	for( i = 0; i < parameters.length; ++ i ) {
+    	
+    		p = parameters[ i ];
+    	
+    		material = new THREE.LineBasicMaterial( { color: p[ 1 ], opacity: p[ 2 ], linewidth: p[ 3 ] } );
+    	
+    		line = new THREE.Line( geometry, material, THREE.LinePieces );
+    		line.scale.x = line.scale.y = line.scale.z = p[ 0 ];
+    		line.originalScale = p[ 0 ];
+    		line.rotation.y = Math.random() * Math.PI;
+    		line.updateMatrix();
+    		scene.add( line );
+    	
+    	}
+    	
+    	renderer = new THREE.WebGLRenderer( { antialias: true } );
+    	renderer.setPixelRatio( window.devicePixelRatio );
+    	renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+    	container.appendChild( renderer.domElement );
+    	
+    	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    	document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+    	document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+    	
+    	window.addEventListener( 'resize', onWindowResize, false );
+	
+	}
+	
+	function onWindowResize() {
+	
+		windowHalfX = window.innerWidth / 2;
+		windowHalfY = window.innerHeight / 2;
+	
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+	
+		renderer.setSize( window.innerWidth, window.innerHeight );
+	
+	}
+	
+	function onDocumentMouseMove( event ) {
+	
+		mouseX = event.clientX - windowHalfX;
+		mouseY = event.clientY - windowHalfY;
+	
+	}
+	
+	function onDocumentTouchStart( event ) {
+	
+    	if ( event.touches.length > 1 ) {
+    	
+    		event.preventDefault();
+    	
+    		mouseX = event.touches[ 0 ].pageX - windowHalfX;
+    		mouseY = event.touches[ 0 ].pageY - windowHalfY;
+    	
+    	}
+	
+	}
+	
+	function onDocumentTouchMove( event ) {
+	
+		if ( event.touches.length == 1 ) {
+	
+			event.preventDefault();
+	
+			mouseX = event.touches[ 0 ].pageX - windowHalfX;
+			mouseY = event.touches[ 0 ].pageY - windowHalfY;
+	
+	    }
+	
+	}
+	
+	function animate() {
+	
+		requestAnimationFrame( animate );
+	
+		render();
+	
+	}
+	
+	function render() {
+		camera.position.y += ( - mouseY + 200 - camera.position.y ) * .05;
+		camera.lookAt( scene.position );
+	
+		renderer.render( scene, camera );
+	
+		var time = Date.now() * 0.0001;
+	
+		for ( var i = 0; i < scene.children.length; i ++ ) {
+			var val = audioSource.streamData[i];
+			val /= 255;
+			var object = scene.children[ i ];
+			if ( object instanceof THREE.Line ) {
+				object.rotation.y = time * ( i < 4 ? ( i + 1 ) : - ( i + 1 ) );
+				if(val > 0.80) {
+					object.scale.x = object.scale.y = object.scale.z = object.originalScale + val*((i/5+1) * (1 + 0.5 * Math.sin( 7*time ) ) * 3);
+				}
+				else if(val < 0.20) {
+					object.scale.x = object.scale.y = object.scale.z = object.originalScale + (1/val)*((i/5+1) * (1 + 0.5 * Math.sin( 7*time ) ) * 3);
+				}
+				else {
+					object.scale.x = object.scale.y = object.scale.z = object.originalScale + ((i/5+1) * (1 + 0.5 * Math.sin( 7*time ) ));
+				}
+			}
+	    // Schedule next animation frame
+        requestAnimationFrame(draw);
+	}
+	
+	// Begin stream and drawing runs
+    audioSource.playStream(loader.streamUrl);
+    draw();
+}
